d8a9de2b00b43dc76feee15ad94c7a647e4d1412
Taesong Kim <taesongweb@gmail.com>
2020-12-31 23:54:09 +0900

    BOJ ex9095

---
+++ Backjoon_study1/week02/ex9095.py
@@ -0,0 +1,20 @@
+T = int(input())  # 테스ㅡ 케이스의 개수 T가 주어짐
+sum_list = []
+for i in range(T):
+    n = int(input())
+    sum_list.append(n)
+
+
+def oneTwoThreeSum(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    else:
+        return oneTwoThreeSum(n-3) + oneTwoThreeSum(n-2) + oneTwoThreeSum(n-1)
+
+
+for k in sum_list:
+    print(oneTwoThreeSum(k))
