68392892d81026b95fefb1e041ae7ac2643214fc
Taesong Kim <taesongweb@gmail.com>
2021-03-13 12:06:39 +0900

    Programmers SQL Level2

---
+++ programmers/SQL/Level2/동물수구하기.sql
@@ -0,0 +1,2 @@
+SELECT count(*) as count
+FROM ANIMAL_INS
---
+++ programmers/SQL/Level2/루시와엘라찾기.sql
@@ -0,0 +1,3 @@
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME ='LUCY' or NAME ='Ella' or NAME ='Pickle' or NAME ='Rogan' or NAME ='Sabrina' or NAME ='Mitty'
