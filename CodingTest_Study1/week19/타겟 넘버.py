6109ea1196127414a9dc59d4870d414663bf7e12
Taesong Kim <taesongweb@gmail.com>
2021-04-29 17:26:35 +0900

    CodingTest_Stduy1 week19 Programmers 타겟 넘버

--- CodingTest_Study1/week19/타겟 넘버.py
+++ CodingTest_Study1/week19/타겟 넘버.py
@@ -1,18 +1,9 @@
-def solution(phone_book):
-    answer = True
-    phone = {}  # 딕셔너리 형태
-
-    # 전화번호부를 딕셔너리형으로 저장
-    for i in phone_book:
-        phone[i] = 1
-
-    for k in phone_book:
-        tmp = ""
-        # 전화번호부의 번호 하나씩 조사 ex) k : 110 num: 1, 1, 0순으로 조사
-        for num in k:
-            tmp += num
-            # 일부가 전화번호부 번호에 포함되면 answer flag를 False 변경
-            if tmp in phone and tmp != k:
-                answer = False
-                return answer
-    return answer
+def solution(numbers: list, target: int):
+    sub_list = [0]
+    for i in numbers:
+        temp_list = []
+        for j in sub_list:
+            temp_list.append(j+i)
+            temp_list.append(j-i)
+        sub_list = temp_list
+    return sub_list.count(target)
