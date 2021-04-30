f4ce0f7655e721fb2c10d689ccc6595721043b22
Taesong Kim <taesongweb@gmail.com>
2021-04-30 16:05:16 +0900

    Update CodingTest_Stduy1 week18 Programmers 더맵게

--- CodingTest_Study1/week18/더맵게.py
+++ CodingTest_Study1/week18/더맵게.py
@@ -1,18 +1,16 @@
 import heapq
 
 
-def solution(scv, K):
-    answer = 0
-    h = []
-
-    for i in scv:
-        heapq.heappush(h, i)
-
-    while h[0] < K:
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    cnt = 0
+    while scoville[0] < K:
         try:
-            heapq.heappush(h, heapq.heappop(h)+(heapq.heappop(h)*2))
+            mix_sco = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
         except IndexError:
             return -1
-        answer += 1
 
-    return answer
+        heapq.heappush(scoville, mix_sco)
+
+        cnt += 1
+    return cnt
