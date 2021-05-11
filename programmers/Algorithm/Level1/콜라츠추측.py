e2c2e846e2d1c1ce62de7a367fa29bc43e953493
Taesong Kim <taesongweb@gmail.com>
2021-05-11 23:50:04 +0900

    Programmers Algorithm Level1 콜라츠추측

---
+++ programmers/Algorithm/Level1/콜라츠추측.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = 0
+    while num != 1 and answer <= 500:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        answer += 1
+    if answer > 500:
+        return -1
+    return answer
